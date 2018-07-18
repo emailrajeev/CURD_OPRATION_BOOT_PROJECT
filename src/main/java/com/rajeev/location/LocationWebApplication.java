e5d60cb44b56ad9b596ebc3b1f5ac2b4ed26d14a
“[Rajeev Ranjan]" <[email.rajeevranjan@gmail.com]>
2018-07-18 21:33:58 +0530

    Success

--- src/main/java/com/rajeev/location/LocationWebApplication.java
+++ src/main/java/com/rajeev/location/LocationWebApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LocationWebApplication {
-
+/**
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(LocationWebApplication.class, args);
 	}
